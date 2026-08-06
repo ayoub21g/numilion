cbb8b6a0fe850e26dc6d502b8f6d2eb6eb0771e4
numilion <ayoubghe2010@gmail.com>
2026-08-06 19:42:12 +0200

    fix: rebuild avec noms d'assets sans caractères spéciaux

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/numilion/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/app.[hash].js',
+        chunkFileNames: 'assets/chunk.[hash].js',
+        assetFileNames: 'assets/asset.[hash].[ext]',
+      },
+    },
+  },
 });
